eb00071e53d622901957ae0d652f26c3a7abb737
Зражевский Евгений Геннадьевич (4051458) <zrazhevskii@vtb.ru>
2023-03-07 21:10:03 +0300

    Updated background

--- libs/utils.js
+++ libs/utils.js
@@ -1,7 +1,6 @@
 function findAsset(assets, pattern) {
     for (const asset of assets) {
         if (asset.name.match(pattern)) {
-            console.log("asset found by name: " + pattern + "; " + JSON.stringify(asset));
             return asset;
         }
     }
